dd2a363150664aebd68065d9dee90efefc1c2f9b
ChrisAchinga <achinga.chris@gmail.com>
2020-07-23 14:27:14 +0300

    del

--- catalog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Room, RoomInstance, Occupant, Type
-
-admin.site.register(Room)
-admin.site.register(RoomInstance)
-admin.site.register(Occupant)
-admin.site.register(Type)
