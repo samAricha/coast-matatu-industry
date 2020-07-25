283fbbcf6bb0dc6904c2f4304e6c2799ab1663fd
ChrisAchinga <achinga.chris@gmail.com>
2020-07-25 19:33:17 +0300

    added apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed applications
+    'rest_framework',
+
+    # my application
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
